d69bcd70a8c951ba25a355d6254290df47e40855
Martijn Mulder <Martijn.Mulder@vitens.nl>
2020-03-23 13:24:28 +0100

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(name='meetdoelenproject',
-      version='0.2',
+      version='0.3',
       description='Meetdoelenproject ter optimalisatie van Vitens-meetnet',
-      url='https://github.com/mgjmulder/Vitens-Meetdoelenproject',
+      url='https://github.com/mgjmulder/Vitens-Meetdoelenproject/',
       author='Martijn Mulder',
       author_email='martijn.mulder@vitens.nl',
       packages=['meetdoelenproject'],
